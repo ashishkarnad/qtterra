e5e0ad07ec4c24d6ff5102589fd6a3d9de9be64e
ashish <ashish@oldlaptop>
2020-10-20 23:22:20 +0530

    added 4 subnets

--- main.tf
+++ main.tf
@@ -3,16 +3,48 @@ resource "aws_vpc" "ashishvpc"
     cidr_block = "192.168.0.0/16"
     enable_dns_support = "true"
     tags = {
-        Name = "frontf"
+        Name = "qtvpc"
     }
 }
 
 resource "aws_s3_bucket" "ashishbucket" {
 bucket = "ashishawsknewltf"
 tags = {
-    Name = "ashishawskltf"
+    Name = "ashishawsoldltp"
      Environment = "Dev"
      CreatedBy = "Terraform"
       }
     
 }   
+
+resource "aws_subnet" "web" {
+    vpc_id = aws_vpc.ashishvpc.id 
+    cidr_block = "192.168.0.0/24"
+    tags = {
+        Name = "web"
+    }
+}
+
+resource "aws_subnet" "app" {
+    vpc_id = aws_vpc.ashishvpc.id 
+    cidr_block = "192.168.1.0/24"
+    tags = {
+        Name = "app"
+    }
+}
+
+resource "aws_subnet" "mgmt" {
+    vpc_id = aws_vpc.ashishvpc.id 
+    cidr_block = "192.168.2.0/24"
+    tags = {
+        Name = "mgmt"
+    }
+}
+
+resource "aws_subnet" "db" {
+    vpc_id = aws_vpc.ashishvpc.id 
+    cidr_block = "192.168.3.0/24"
+    tags = {
+        Name = "db"
+    }
+}
