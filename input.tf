8a747235da3f9bc6386d88ee1bd0064ecb180a33
ashish <ashish@oldlaptop>
2020-10-21 13:53:01 +0530

    added input to make it more reusable code

---
+++ input.tf
@@ -0,0 +1,14 @@
+#variable for region
+variable region {
+    type = string
+    default = "us-west-2"
+    description = "target region for creation of networ"
+
+}
+
+#variable for vpc_cidr
+variable vpc_cidr {
+    type = string
+    default = "192.168.0.0/16"
+    description = "cidr range"
+}
--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "ashishvpc" {
-    cidr_block = "192.168.0.0/16"
+    cidr_block = var.vpc_cidr
     enable_dns_support = "true"
     tags = {
         Name = "ashishvpc"
